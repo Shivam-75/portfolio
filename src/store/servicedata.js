be7ee75329aa4383a1f1b1a9316b968e0c90179f
Shivam-75 <shivampandey7518200305@gmail.com>
2025-07-08 21:50:01 +0530

    last update

--- src/store/servicedata.js
+++ src/store/servicedata.js
@@ -1,38 +1,38 @@
 export const servicedata = [
     {
-        img: "./public/vite.svg",
-        title: "web Desgin",
+        img: "/vite.svg",
+        title: "Web Design",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maioresquis assumenda doloremque aliquam sed, asperiores velit voluptatemne cessitatibus in repellat dolor amet iste deleniti vitae praesentium eos veritatis architecto!",
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maiores quis assumenda doloremque aliquam sed, asperiores velit voluptatem necessitatibus in repellat dolor amet iste deleniti vitae praesentium eos veritatis architecto!",
     },
     {
-        img: "./public/vite.svg",
-        title: "web Desgin",
+        img: "/vite.svg",
+        title: "Web Design",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maioresquis assumenda doloremque aliquam sed, asperiores velit voluptatemne cessitatibus in repellat dolor amet iste deleniti vitae praesentium eos veritatis architecto!",
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maiores quis assumenda doloremque aliquam sed, asperiores velit voluptatem necessitatibus in repellat dolor amet iste deleniti vitae praesentium eos veritatis architecto!",
     },
     {
-        img: "./public/vite.svg",
-        title: "web Desgin",
+        img: "/vite.svg",
+        title: "Web Design",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maioresquis assumenda doloremque aliquam sed, asperiores velit voluptatemne cessitatibus in repellat dolor amet iste deleniti vitae praesentium eos veritatis architecto!",
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maiores quis assumenda doloremque aliquam sed, asperiores velit voluptatem necessitatibus in repellat dolor amet iste deleniti vitae praesentium eos veritatis architecto!",
     },
     {
-        img: "./public/vite.svg",
-        title: "web Desgin",
+        img: "/vite.svg",
+        title: "Web Design",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maioresquis assumenda doloremque aliquam sed, asperiores velit voluptatemne cessitatibus in repellat dolor amet iste deleniti vitae praesentium eos veritatis architecto!",
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maiores quis assumenda doloremque aliquam sed, asperiores velit voluptatem necessitatibus in repellat dolor amet iste deleniti vitae praesentium eos veritatis architecto!",
     },
     {
-        img: "./public/vite.svg",
-        title: "web Desgin",
+        img: "/vite.svg",
+        title: "Web Design",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maioresquis assumenda doloremque aliquam sed, asperiores velit voluptatemne cessitatibus in repellat dolor amet iste deleniti vitae praesentium eos veritatis architecto!",
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maiores quis assumenda doloremque aliquam sed, asperiores velit voluptatem necessitatibus in repellat dolor amet iste deleniti vitae praesentium eos veritatis architecto!",
     },
     {
-        img: "./public/vite.svg",
-        title: "web Desgin",
+        img: "/vite.svg",
+        title: "Web Design",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maioresquis assumenda doloremque aliquam sed, asperiores velit voluptatemne cessitatibus in repellat dolor amet iste deleniti vitae praesentium eos veritatis architecto!",
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maiores quis assumenda doloremque aliquam sed, asperiores velit voluptatem necessitatibus in repellat dolor amet iste deleniti vitae praesentium eos veritatis architecto!",
     },
 ];
